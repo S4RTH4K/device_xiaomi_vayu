081e35f85dbe0c6ba69a284c46e66ded54cf1c83
S4RTH4K <sarthak0311@gmail.com>
2021-08-08 08:05:01 +0000

    vayu: adapt for POSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vayu.mk
+    $(LOCAL_DIR)/potato_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-user \
-    lineage_vayu-userdebug \
-    lineage_vayu-eng
+    potato_vayu-user \
+    potato_vayu-userdebug \
+    potato_vayu-eng
